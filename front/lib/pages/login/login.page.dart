f444a5707dd5da11a4cd7e6f73476f8fe255e6e4
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-04-23 14:29:12 -0300

    REFACTOR: Creating sub components and utilizing glolbal styles

--- front/lib/pages/login/login.page.dart
+++ front/lib/pages/login/login.page.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'package:front/global.style.dart';
+
 import './login.styles.dart';
 import 'package:flutter/material.dart';
 
@@ -12,18 +13,30 @@ class _LoginPageState extends State<LoginPage> {
   String _password;
   bool _isValidating = false;
 
-  final linearGradient = LinearGradient(colors: [
-    Color.fromRGBO(100, 100, 94, 0.695),
-    Color.fromRGBO(73, 72, 74, 0.695),
-  ], stops: [
-    0.0,
-    0.695
-  ], transform: GradientRotation(2.13959913 * pi));
+  setEmail(text) {
+    setState(() {
+      _email = text;
+    });
+  }
+
+  setPassword(text) {
+    setState(() {
+      _password = text;
+    });
+  }
+
+  setValidating(text) {
+    setState(() {
+      _isValidating = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('CTracker'),
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(Icons.arrow_back_rounded),
           tooltip: 'Go Back',
@@ -34,22 +47,10 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: styles['appbar-background'],
       ),
       body: _isValidating
-          ? Container(
-              decoration: BoxDecoration(gradient: linearGradient),
-              child: Center(
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: CircularProgressIndicator(
-                    strokeWidth: 5,
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                        Color.fromRGBO(50, 50, 50, 1)),
-                  ),
-                ),
-              ),
-            )
+          ? _progressIndicator()
           : Container(
-              decoration: BoxDecoration(gradient: linearGradient),
+              decoration:
+                  BoxDecoration(gradient: GlobalStyles.standardGradient),
               child: Center(
                 child: SingleChildScrollView(
                   child: Column(
@@ -58,57 +59,11 @@ class _LoginPageState extends State<LoginPage> {
                         padding: const EdgeInsets.only(bottom: 50),
                         child: Text(
                           'Welcome to CTracker App!',
-                          style: styles['welcome-text'],
+                          style: GlobalStyles.boldText,
                         ),
                       ),
-                      Wrap(
-                        spacing: 10,
-                        direction: Axis.vertical,
-                        children: [
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: TextField(
-                                onChanged: (text) {
-                                  setState(() {
-                                    _email = text;
-                                  });
-                                },
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'Email')),
-                          ),
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.8,
-                            child: TextField(
-                                onChanged: (text) {
-                                  setState(() {
-                                    _password = text;
-                                  });
-                                },
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(),
-                                    hintText: 'Password')),
-                          )
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: ElevatedButton(
-                          onPressed: () =>
-                              setState(() => {_isValidating = true}),
-                          child: Text('Login'),
-                          style: styles['button'],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10),
-                        child: ElevatedButton(
-                          onPressed: () => setState(() => {}),
-                          child: Text('Cancel'),
-                          style: styles['button'],
-                        ),
-                      )
+                      _userInputs(context, setPassword, setEmail),
+                      _buttons(setValidating)
                     ],
                   ),
                 ),
@@ -116,4 +71,69 @@ class _LoginPageState extends State<LoginPage> {
             ),
     );
   }
+}
+
+_progressIndicator() {
+  return Container(
+    decoration: BoxDecoration(gradient: GlobalStyles.standardGradient),
+    child: Center(
+      child: SizedBox(
+        height: 100,
+        width: 100,
+        child: CircularProgressIndicator(
+          strokeWidth: 5,
+          valueColor: AlwaysStoppedAnimation<Color>(
+              GlobalStyles.rgbColors['dark-gray']),
+        ),
+      ),
+    ),
+  );
+}
+
+_userInputs(
+  context,
+  setPassword,
+  setEmail,
+) {
+  return Wrap(
+    spacing: 10,
+    direction: Axis.vertical,
+    children: [
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: TextField(
+            onChanged: (text) {
+              setPassword(text);
+            },
+            decoration: GlobalStyles.standardTextField),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: TextField(
+            onChanged: (text) {
+              setEmail(text);
+            },
+            obscureText: true,
+            decoration: GlobalStyles.standardTextField),
+      )
+    ],
+  );
+}
+
+_buttons(setValidating) {
+  return Wrap(spacing: 10, direction: Axis.vertical, children: [
+    Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: ElevatedButton(
+        onPressed: () => setValidating(),
+        child: Text('Login'),
+        style: GlobalStyles.standardButton,
+      ),
+    ),
+    ElevatedButton(
+      onPressed: () => setValidating(),
+      child: Text('Cancel'),
+      style: GlobalStyles.standardButton,
+    )
+  ]);
 }
