0c571d30a89a9d5f58c267a3d474901fbfa8ac79
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-04-24 16:05:16 -0300

    FIX: Fetching data correctly

--- front/lib/pages/home.page.dart
+++ front/lib/pages/home.page.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'dart:async';
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:front/apis/covid-data.api.dart';
+import 'package:front/models/country-data.model.dart';
 import '../global.style.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,64 +10,28 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({@required this.userId, @required this.id, @required this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
-
 class _HomePageState extends State<HomePage> {
-  Future<Album> futureAlbum;
   bool requestError = false;
+  List<CountryData> countries = [];
 
-  Future<Album> fetchAlbum() async {
-    final response =
-        await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
-
-    if (response.statusCode == 200) {
-      // If the server did return a 200 OK response,
-      // then parse the JSON.
-      return Album.fromJson(jsonDecode(response.body));
-    } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
-      throw Exception('Failed to load album');
-    }
-  }
-
-  Future fetchdata() async {
-    final response = await http.get(
-        Uri.https(
-            'covid-19-data.p.rapidapi.com', '/country', {'name': 'italy'}),
-        headers: {
-          'x-rapidapi-key': '5b8957a382msh2d23f669b923e9ep150b64jsn1408a53a8c56'
-        });
-    print(response.body);
+  Future<CountryData> fetchdata() async {
+    final response = await CovidDataAPI().fetchDataByCountry('brazil');
+    final countryData = CountryData.fromJson(jsonDecode(response.body));
+    return countryData;
   }
 
   void initState() {
-    // TODO: implement initState
     super.initState();
     try {
-      // futureAlbum = fetchAlbum();
-      final response = fetchdata();
+      final countryData = fetchdata().then((value) => setState(() {
+            print(value);
+            this.countries.add(value);
+          }));
     } catch (e) {
       setState(() {
         requestError = true;
       });
     }
-
-    print(futureAlbum);
   }
 
   @override
