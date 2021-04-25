5b6bbcd2d2e4aab400ec8587da5fc63c4698f359
Bruno Pedrazza <pedrazzabruno@gmail.com>
2021-04-25 19:10:10 -0300

    Change endpoints url

--- server/ctracker/api/urls.py
+++ server/ctracker/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from ..api import user, place, notify
+from ctracker.api.controllers import placecontroller, notificationcontroller, usercontroller
 
 urlpatterns = [
-    path("user/login", user.login),
-    path("user/register", user.register),
-    path("user/update/<email>", user.update),
-    path("user/<email>", user.get),
-    path("place/register", place.register),
-    path("notify", notify.post),
+    path("user/login", usercontroller.login),
+    path("user/register", usercontroller.register),
+    path("user/update/<email>", usercontroller.update),
+    path("user/<email>", usercontroller.get),
+    path("place/register", placecontroller.register),
+    path("place/user/<user_email>", placecontroller.get_places),
+    path("notification", notificationcontroller.post_notification),
 ]
