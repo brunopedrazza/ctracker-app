92afe54977eb752da2e335a51f6a96a2edfeb0e6
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-06-07 22:18:24 -0300

    FEAT: User model

---
+++ front/lib/models/user.model.dart
@@ -0,0 +1,31 @@
+import 'package:crypto/crypto.dart';
+import 'package:flutter/foundation.dart';
+
+class User {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final Digest password;
+  final String birthday;
+
+  User(
+      {@required this.firstName,
+      @required this.lastName,
+      @required this.email,
+      @required this.password,
+      @required this.birthday});
+
+  // User.fromJson(Map<String, dynamic> json)
+  // : name = json['n'],
+  //   url = json['u'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'first_name': firstName,
+      'last_name': lastName,
+      'birthdate': birthday,
+      'email': email,
+      'password': password.toString(),
+    };
+  }
+}
