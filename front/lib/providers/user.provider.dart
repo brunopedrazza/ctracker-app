5cf2917f090d444563fd2bf44bf764efe98dfe79
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-06-25 17:14:29 -0300

    FEAT: Adding check if user is logged in to provider

--- front/lib/providers/user.provider.dart
+++ front/lib/providers/user.provider.dart
@@ -3,13 +3,19 @@ import 'package:front/models/user.model.dart';
 
 class UserProvider extends ChangeNotifier {
   User _userLoggedIn;
+  bool _isUserLoggedIn = false;
 
   User getUser() {
     return _userLoggedIn;
   }
 
+  bool isLoggedIn() {
+    return _isUserLoggedIn;
+  }
+
   void setUser(User user) {
     _userLoggedIn = user;
+    _isUserLoggedIn = true;
     notifyListeners();
   }
 
