3331b5f8ff35654571967e9e8a337d8a2fca86de
Guilherme Lacerda <lacerdagomes95@gmail.com>
2021-06-27 17:52:29 -0300

    FEAT: Local for home

--- front/lib/localization/localizations.dart
+++ front/lib/localization/localizations.dart
@@ -16,9 +16,12 @@ class AppLocalizations {
       'welcome': 'Welcome to CTracker App!',
       'welcomeName': 'Welcome, ',
       'imInfected': 'I\'m infected!',
-      'covidDataMessage': 'Here are some of the latest COVID-19 statistics. Click on the desired country to see more information.',
-      'disabledNotifyMessage': 'You have recently notified us that you were infected. You must wait at least 30 days until you can notify again.',
-      'latestPlacesMessage': 'Here are some of the latest places you have visited. If you had contact with any infected person, the place will be highlighted in red.',
+      'covidDataMessage':
+          'Here are some of the latest COVID-19 statistics. Click on the desired country to see more information.',
+      'disabledNotifyMessage':
+          'You have recently notified us that you were infected. You must wait at least 30 days until you can notify again.',
+      'latestPlacesMessage':
+          'Here are some of the latest places you have visited. If you had contact with any infected person, the place will be highlighted in red.',
       'registered': 'Successfully registered!',
       'error': 'An error has occurred. Please try again.',
       'email': 'Email',
@@ -39,20 +42,30 @@ class AppLocalizations {
       'birthdateInputValidation': 'Please check your birthdate.',
       'passwordInput': 'Type your password',
       'passwordInputValidation': 'Please check your password.',
-      'reTypePassword': 'Re-type your password'
+      'reTypePassword': 'Re-type your password',
+      'onePlaceNeeded': 'Must have at least one place to notify.',
+      'placesFetchError':
+          'An error occurred while fetching your visited places.',
+      'notificationError': 'An error occurred while trying to notify.',
+      'alreadyNotified': 'You must await 30 days to notify again.',
+      'successfullyNotified': 'Users were notified successfully.',
     },
     'es': {
       'welcome': 'Bienvenido a CTracker!',
       'welcomeName': 'Bienvenido, ',
       'imInfected': 'Estoy infectado!',
-      'covidDataMessage': 'Estas son algunas de las últimas estadísticas de COVID-19. Haga clic en el país deseado para ver más información.',
-      'disabledNotifyMessage': 'Recientemente nos notificó que estaba infectado. Debe esperar al menos 30 días hasta que pueda notificar nuevamente.',
-      'latestPlacesMessage': 'Estos son algunos de los últimos lugares que ha visitado. Si tuvo contacto con alguna persona infectada, el lugar se resaltará en rojo.',
+      'covidDataMessage':
+          'Estas son algunas de las últimas estadísticas de COVID-19. Haga clic en el país deseado para ver más información.',
+      'disabledNotifyMessage':
+          'Recientemente nos notificó que estaba infectado. Debe esperar al menos 30 días hasta que pueda notificar nuevamente.',
+      'latestPlacesMessage':
+          'Estos son algunos de los últimos lugares que ha visitado. Si tuvo contacto con alguna persona infectada, el lugar se resaltará en rojo.',
       'registered': 'Registrado exitosamente!',
       'error': 'Se ha producido un error. Inténtalo de nuevo.',
       'email': 'Correo electrónico',
       'password': 'Contraseña',
-      'invalidEmailPassword': 'Correo electrónico o contraseña no válidos. Inténtalo de nuevo.',
+      'invalidEmailPassword':
+          'Correo electrónico o contraseña no válidos. Inténtalo de nuevo.',
       'login': 'Entrar',
       'signUp': 'Inscribirse',
       'signUpMessage': '¡Regístrese en nuestra plataforma!',
@@ -74,14 +87,18 @@ class AppLocalizations {
       'welcome': 'Bem-vindo ao CTracker!',
       'welcomeName': 'Bem-vindo, ',
       'imInfected': 'Estou infectado!',
-      'covidDataMessage': 'Aqui estão algumas das estatísticas mais recentes do COVID-19. Clique no país desejado para ver mais informações.',
-      'disabledNotifyMessage': 'Você nos notificou recentemente que estava infectado. Por favor, espere pelo menos 30 dias para poder notificar novamente.',
-      'latestPlacesMessage': 'Aqui estão alguns dos lugares mais recentes que você visitou. Se você teve contato com alguma pessoa infectada, o local ficará destacado em vermelho.',
+      'covidDataMessage':
+          'Aqui estão algumas das estatísticas mais recentes do COVID-19. Clique no país desejado para ver mais informações.',
+      'disabledNotifyMessage':
+          'Você nos notificou recentemente que estava infectado. Por favor, espere pelo menos 30 dias para poder notificar novamente.',
+      'latestPlacesMessage':
+          'Aqui estão alguns dos lugares mais recentes que você visitou. Se você teve contato com alguma pessoa infectada, o local ficará destacado em vermelho.',
       'registered': 'Registrado com sucesso!',
       'error': 'Acorreu um erro. Por favor, tente novamente.',
       'email': 'Email',
       'password': 'Senha',
-      'invalidEmailPassword': 'E-mail ou senha incorretos. Por favor, tente novamente.',
+      'invalidEmailPassword':
+          'E-mail ou senha incorretos. Por favor, tente novamente.',
       'login': 'Entrar',
       'signUp': 'Inscrever-se',
       'signUpMessage': 'Cadastre-se em nossa plataforma!',
@@ -97,7 +114,15 @@ class AppLocalizations {
       'birthdateInputValidation': 'Verifique sua data de nascimento.',
       'passwordInput': 'Digite sua senha',
       'passwordInputValidation': 'Por favor, verifique sua senha.',
-      'reTypePassword': 'Digite novamente sua senha'
+      'reTypePassword': 'Digite novamente sua senha',
+      'onePlaceNeeded':
+          'Deve ter cadastrado ao menos um estabelecimento para notificar.',
+      'placesFetchError':
+          'Ocorreu um erro ao buscar seus estabelecimnentos visitados.',
+      'notificationError': 'Ocorreu um erro ao notificar.',
+      'alreadyNotified':
+          'Você deve aguardar 30 dias até poder notificar novamente.',
+      'successfullyNotified': 'Usuários foram notificados com sucesso.',
     },
   };
 
@@ -209,13 +234,33 @@ class AppLocalizations {
     return _localizedValues[locale.languageCode]['reTypePassword'];
   }
 
+  String get onePlaceNeeded {
+    return _localizedValues[locale.languageCode]['onePlaceNeeded'];
+  }
+
+  String get placesFetchError {
+    return _localizedValues[locale.languageCode]['placesFetchError'];
+  }
+
+  String get notificationError {
+    return _localizedValues[locale.languageCode]['notificationError'];
+  }
+
+  String get alreadyNotified {
+    return _localizedValues[locale.languageCode]['alreadyNotified'];
+  }
+
+  String get successfullyNotified {
+    return _localizedValues[locale.languageCode]['successfullyNotified'];
+  }
 }
 
 class AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
   const AppLocalizationsDelegate();
 
   @override
-  bool isSupported(Locale locale) => ['en', 'es', 'pt'].contains(locale.languageCode);
+  bool isSupported(Locale locale) =>
+      ['en', 'es', 'pt'].contains(locale.languageCode);
 
   @override
   Future<AppLocalizations> load(Locale locale) {
